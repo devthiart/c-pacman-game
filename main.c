22228cc2c28b078459de3e7088a9826b9a3a758a
Thiago Nunes <devthiart@gmail.com>
2025-11-05 12:15:33 -0300

    Reading map file

--- main.c
+++ main.c
@@ -1,13 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
-	char map[5][10];
+	// Matrix of 5x10
+	// 10+1 because every string ends with /0
+	char map[5][10 + 1];
 
-	map[0][0] = '|';
-	map[4][9] = '@';
+	FILE *f;
+	f = fopen("map.txt", "r");
+	if (f == 0)
+	{
+		printf("Error reading file");
+		exit(1);
+	}
 
-	printf("%c %c", map[0][0], map[4][9]);
+	for (int i = 0; i < 5; i++)
+	{
+		fscanf(f, "%s", map[i]);
+	}
+
+	printf("Printing by string:\n");
+	for (int i = 0; i < 5; i++)
+	{
+		printf("%s\n", map[i]);
+	}
+
+	printf("Printing by char:\n");
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			printf("%c", map[i][j]);
+		}
+		printf("\n");
+	}
 
 	return 0;
 }
